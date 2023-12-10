6eabfb042bcc943350572f4312cb0d39e4476723
miwanoff <marina.iva.noffa@gmail.com>
2023-12-10 13:36:43 +0200

    Add loader

--- script.js
+++ script.js
@@ -4,7 +4,18 @@ window.onload = function () {
   let menuBottom = 100; // значення відступу від верхнього краю при прокрутці
   window.onscroll = function () {
     if (document.documentElement.scrollTop > 200) {
-      if (imgDin.classList.contains("fade")) imgDin.classList.remove("fade");
+      if (imgDin.classList.contains("fade")) {
+        imgDin.classList.remove("fade");
+      }
+    }
+    if (document.documentElement.scrollTop < 300) {
+      imgDin.src = "./images/wizard.png";
+      imgDin.style.left = "400px";
+      imgDin.style.bottom = "400px";
+    }
+    if (document.documentElement.scrollTop > 600) {
+      imgDin.src = "./images/портер-21774949.jpg";
+      imgDin.style.bottom = "40px";
     }
     if (document.documentElement.scrollTop < menuBottom) {
       // або document.body.scrollTop > menuBottom
@@ -13,4 +24,15 @@ window.onload = function () {
       nav.classList.add("fixed"); // додаємо клас 'fixed'
     }
   };
+
+  let state = document.readyState;
+  if (state == "interactive") {
+    document.getElementById("wrap").style.visibility = "hidden";
+    document.getElementById("loader").style.visibility = "visible";
+  } else if (state == "complete") {
+    setTimeout(function () {
+      document.getElementById("loader").style.visibility = "hidden";
+      document.getElementById("wrap").style.visibility = "visible";
+    }, 1000);
+  }
 };
