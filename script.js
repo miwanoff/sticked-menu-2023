6f64e5149e6bb9dd747d31a994471003576ee5e2
miwanoff <marina.iva.noffa@gmail.com>
2023-12-10 13:11:34 +0200

    Add fade class

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 window.onload = function () {
+  let imgDin = document.getElementById("din");
   let nav = document.querySelector("nav");
   let menuBottom = 100; // значення відступу від верхнього краю при прокрутці
   window.onscroll = function () {
+    if (document.documentElement.scrollTop > 200) {
+      if (imgDin.classList.contains("fade")) imgDin.classList.remove("fade");
+    }
     if (document.documentElement.scrollTop < menuBottom) {
       // або document.body.scrollTop > menuBottom
       nav.classList.remove("fixed"); // видаляємо клас 'fixed'
